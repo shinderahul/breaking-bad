a818b6258caf576a42c3c6c61856aed650602166
Rahul <rahul.shinde@digitas.com>
2020-07-13 19:07:24 +0530

    Iterate item

--- src/components/characters/CharacterGrid.js
+++ src/components/characters/CharacterGrid.js
@@ -5,7 +5,11 @@ const CharacterGrid = ({ items, isLoading }) => {
 	return isLoading ? (
 		<h1>Loading...</h1>
 	) : (
-		<section className='cards'>Test</section>
+		<section className='cards'>
+			{items.map((item) => (
+				<h1>{item.name}</h1>
+			))}
+		</section>
 	);
 };
 
